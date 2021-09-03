ab1a22c0c45662c20ead3b800d3d41d88a4ef634
Andy Dang <andy.dang.it@gmail.com>
2021-09-02 21:37:21 -0500

    Added Router / Switch / Route

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,30 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Home from '../Pages/Home'
-import About from '../Pages/About'
-import Contact from '../Pages/Contact'
-import Blog from '../Pages/Blog'
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Blog from "../Pages/Blog";
 
 function App() {
   return (
     <Router>
-      <Route exact path='/'>
-        <Home/>
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route  path='/about'>
-        <About/>
-      </Route>
+        <Route path="/about">
+          <About />
+        </Route>
 
-      <Route  path='/contact'>
-        <Contact/>
-      </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
 
-      <Route  path='/blog'>
-        <Blog/>
-      </Route>
-
+        <Route path="/blog">
+          <Blog />
+        </Route>
+      </Switch>
     </Router>
   );
 }
