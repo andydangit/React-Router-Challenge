fa5fc02b272b3cd8c5c0898a2792512e70555d6b
Andy Dang <andy.dang.it@gmail.com>
2021-09-02 21:52:42 -0500

    Add link tag

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
     return (
-        <div>
-            Navbar  
-        </div>
+       <ul>
+           <li>
+               <Link to="/">Home</Link>
+           </li>
+           <li>
+               <Link to="/about">About</Link>
+           </li>
+           <li>
+               <Link to="/contact">Contact</Link>
+           </li>
+           <li>
+               <Link to="/blog">Blog</Link>
+           </li>
+       </ul>
     )
 }
 
