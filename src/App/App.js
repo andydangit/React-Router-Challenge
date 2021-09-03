884bb9a58b3f88db421c4f03f1d4b90ba0494ce9
Andy Dang <andy.dang.it@gmail.com>
2021-09-02 21:19:34 -0500

    import pages to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from '../Pages/Home'
+import About from '../Pages/About'
+import Contact from '../Pages/Contact'
+import Blog from '../Pages/Blog'
 
 function App() {
   return (
