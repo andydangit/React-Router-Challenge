03e728086398e1363a4a929fc7f4f908f60f1289
Andy Dang <andy.dang.it@gmail.com>
2021-09-02 21:14:43 -0500

    Add Blog page

---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Blog() {
+    return (
+        <div>
+            <h1> Blog Page </h1> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam aliquid sequi sit aspernatur saepe a quaerat nemo impedit. Facere, hic!
+        </div>
+    )
+}
+
+export default Blog
