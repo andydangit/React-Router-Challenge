6369b08628b527ec074be5ea43bd295841352060
Andy Dang <andy.dang.it@gmail.com>
2021-09-02 21:11:37 -0500

    inital files

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1> About page </h1> 
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi magnam odit voluptatum enim sunt unde quod quaerat corrupti placeat, amet praesentium rerum, ad laborum non expedita quas voluptates ducimus perspiciatis aliquid, iste nam qui vel. At, odio molestias dolor sunt dicta maxime sed ipsum aperiam hic odit quia animi praesentium.
+        </div>
+    )
+}
+
+export default About
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1> Home </h1> 
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam asperiores quaerat reiciendis similique, porro dignissimos saepe et consectetur, ducimus voluptas sapiente rem eius quidem vel debitis nisi cumque exercitationem, nobis doloribus quo dolores enim. Harum laborum nulla aut accusantium, itaque veritatis iure eum nostrum illo mollitia aperiam? Magnam, officiis explicabo itaque doloribus enim incidunt ut totam dolorem, recusandae natus nesciunt soluta eaque, qui dignissimos voluptatum! Eveniet quasi laudantium delectus temporibus tempora veniam neque iste, obcaecati, veritatis, consequatur velit nisi dolores dolorum commodi rerum amet iusto eos? Voluptas repudiandae blanditiis aut non voluptate maiores aspernatur in, corporis voluptatibus aliquid repellendus itaque?
+        </div>
+    )
+}
+
+export default Home
