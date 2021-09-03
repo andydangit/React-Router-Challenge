1df09444ee7d76705d7e99656d9d76f87ba0f2f2
Andy Dang <andy.dang.it@gmail.com>
2021-09-02 21:12:39 -0500

    Add Contact page

---
+++ Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div>
+            <h1> Contact Page </h1> 
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex itaque aliquam omnis optio quisquam adipisci eligendi dicta, deserunt, minima ipsum repellat vero quos officia quidem.
+        </div>
+    )
+}
+
+export default Contact
