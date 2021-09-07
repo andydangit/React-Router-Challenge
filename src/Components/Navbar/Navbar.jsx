95628be19479bcfb6c38a51c683a76895ded55f9
pandoragear <andy.dang.it@gmail.com>
2021-09-07 14:31:05 -0500

    added button to show if user is log in

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,28 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthContext";
 import "./Navbar.css";
 
 function Navbar() {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
   return (
     <nav>
       <NavLink className="logo" to="/">
-        React Router 
+        React Router
       </NavLink>
 
       <div className="nav-links">
-        <NavLink activeClassName="selectedPage"  className="link" to="/about">
+        <NavLink activeClassName="selectedPage" className="link" to="/about">
           About
         </NavLink>
-        <NavLink activeClassName="selectedPage"  className="link" to="/contact">
+        <NavLink activeClassName="selectedPage" className="link" to="/contact">
           Contact
         </NavLink>
-        <NavLink activeClassName="selectedPage"  className="link" to="/blog">
+        <NavLink activeClassName="selectedPage" className="link" to="/blog">
           Blog
         </NavLink>
+        <button onClick={() => setIsLoggedIn(!isLoggedIn)}>{isLoggedIn? "logout" : "login"} </button>
       </div>
     </nav>
   );
