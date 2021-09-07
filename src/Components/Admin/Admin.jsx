facef1dc262489e7ec62b1d5a7fc70e1b5f66f9d
pandoragear <andy.dang.it@gmail.com>
2021-09-07 15:08:23 -0500

    only log in user can view the page

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+import {Redirect} from 'react-router-dom'
 
 function Admin() {
+    const {isLoggedIn} = useContext(AuthContext)
+    
+    if(!isLoggedIn) {
+        return <Redirect to="/" />
+    }
+
   return (
     <div>
       <h1>Admin </h1>
